9d7c1a036508546972269c09d5c97a69efb90d32
Joseph Efrain Camargo <efrain.camargo@cbqasolutions.com>
2023-08-19 09:47:35 -0600

    add:End-Poi get hour by id

--- src/routes/hourxhourRoutes.ts
+++ src/routes/hourxhourRoutes.ts
@@ -2,12 +2,15 @@ import express from "express";
 import {
   insertMustAndGetID,
   updateHourxHour,
+  getHourxHourById,
 } from "../controllers/hourxhourController";
 const router = express.Router();
 
 //Insert Must and Get ID
 router.post("/mustAndGetID", insertMustAndGetID);
-//Update HourXHour
+//Update Hour
 router.post("/updateHour", updateHourxHour);
+//Get Hour by ID
+router.get("/getHour/:id", getHourxHourById);
 
 export default router;
